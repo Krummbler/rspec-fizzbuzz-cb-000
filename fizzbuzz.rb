633071983a528224c024e6f9a4ca671e426481c6
Brian Krumm <bjkrumm1@gmail.com>
2018-03-09 00:59:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,11 @@
 def fizzbuzz(beep)
-  if beep % 15 == 0
+  if x % 15 == 0
     return "FizzBuzz"
+  elsif x % 3 == 0
+    return "Fizz"
+  elsif x % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
 end
